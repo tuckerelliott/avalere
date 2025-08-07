f863f226eb305104894af2e40dd22702a3a45f51
Tucker Elliott <telliott@adobe.com>
2025-08-07 11:22:31 -0400

    general styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -183,7 +183,7 @@ export function moveInstrumentation(from, to) {
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
-  await loadCSS("${window.hlx.codeBasePath}/styles/fonts.css");
+  await loadCSS("${window.aem.codeBasePath}/styles/fonts.css");
   try {
     if (!window.location.hostname.includes("localhost"))
       sessionStorage.setItem("fonts-loaded", "true");
